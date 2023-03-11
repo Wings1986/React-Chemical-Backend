86b94a3e02dcaf44e2ccbc7d9d5a6db82463bb0d
hrr2000 <thedragonea@gmail.com>
2023-03-11 23:06:50 +0200

    fix vercel.json

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose
   .then(() => console.log("mongodb connected!!!!"))
   .catch((err) => console.log(err));
 
-app.get('/', () => {
+app.get('/', (req, res) => {
   return res.send('working!!');
 })
 app.use("/", userRoutes);

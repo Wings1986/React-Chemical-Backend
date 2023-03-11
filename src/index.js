6ac74a66c6a4c85b4824a1d7dad3c2e4590a3a9b
hrr2000 <thedragonea@gmail.com>
2023-03-11 23:47:12 +0200

    add build

--- src/index.js
+++ src/index.js
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
 // app.use("/", userRoutes);
 // app.use('/', chemicalRoutes)
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server is running on port ${port}!!!`));

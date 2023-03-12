f819d6779df4415de04fb1581f3baa8b1a79d8ab
hrr2000 <thedragonea@gmail.com>
2023-03-12 04:38:20 +0200

    ready for render

--- build/routes/chemicalRoutes.js
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-// import {postChemical, getChemicals, deleteChemical} from '../controllers/ChemicalController.js'
-// import { isAuth } from '../utils/utils.js';
-// const router = express.Router();
-// router.get("/chemicals", isAuth, getChemicals)
-// router.post("/chemicals", isAuth,   postChemical)
-// router.delete("/chemicals/:id", isAuth, deleteChemical)
-// export default router;
-"use strict";
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// import userRoutes from "../routes/userRoutes.js";
-// import chemicalRoutes from '../routes/chemicalRoutes.js'
-import app from '../api/index'
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+import chemicalRoutes from './routes/chemicalRoutes.js'
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 
-// const app = express();
+const app = express();
 app.use(cors());
 dotenv.config();
 
@@ -14,17 +14,17 @@ app.use(bodyParser.json());
 
 mongoose
   .connect(
-    "mongodb+srv://demariogibson:Aurora1129@cluster0.ncd3l.mongodb.net/chemical_app?retryWrites=true&w=majority",
+    "mongodb+srv://goldsilvermobile:vA56XRNA4ZZUrejG@cluster0.sqcp3fl.mongodb.net/?retryWrites=true&w=majority",
     { useUnifiedTopology: true, useNewUrlParser: true }
   )
   .then(() => console.log("mongodb connected!!!!"))
   .catch((err) => console.log(err));
 
-app.get('/api/', (req, res) => {
+app.get("/", (req, res) => {
   return res.send('working!!');
-})
-// app.use("/", userRoutes);
-// app.use('/', chemicalRoutes)
+});
+app.use("/", userRoutes);
+app.use('/', chemicalRoutes)
 
 const port = process.env.PORT || 5000;
 
